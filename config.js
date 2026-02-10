43a369e92a9f0c1a53529e7330d3b02ade6f04e3
0leksiimarich <oleksijmaric20@gmail.com>
2026-02-10 15:55:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
-// Отримайте API ключ у Google AI Studio: https://aistudio.google.com/
+const firebaseConfig = {
+  apiKey: "AIzaSyCfOnjjk3OcF68LP1bu72BAQ1MOMBeqP0s",
+  authDomain: "ai-frends.firebaseapp.com",
+  projectId: "ai-frends",
+  storageBucket: "ai-frends.firebasestorage.app",
+  messagingSenderId: "155846055941",
+  appId: "1:155846055941:web:2149c41dba6868f01f246c",
+  measurementId: "G-QYEX7HXPCL"
+};
+
 const CONFIG = {
     GEMINI_API_KEY: "AIzaSyBo8mSbCsk9laM1ErdrqYVlkESwdtwHBp4",
     API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 };
-
-export default CONFIG;
