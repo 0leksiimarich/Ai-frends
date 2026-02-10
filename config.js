58730ab51131e52367fc1a839ddcf3bf316a8985
0leksiimarich <oleksijmaric20@gmail.com>
2026-02-10 15:37:29 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Отримайте API ключ у Google AI Studio: https://aistudio.google.com/
+const CONFIG = {
+    GEMINI_API_KEY: "AIzaSyBo8mSbCsk9laM1ErdrqYVlkESwdtwHBp4",
+    API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
+};
+
+export default CONFIG;
